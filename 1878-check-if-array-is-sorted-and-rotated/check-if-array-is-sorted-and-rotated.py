2a268aa11d169008ba2a214448bf8b21828c6841
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-02-17 17:19:45 +0000

    Time: 0 ms (100.00%) | Memory: 12.5 MB (17.05%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -5,11 +5,11 @@ class Solution(object):
         :rtype: bool
         """
         # array is sorted 
-        n = len(nums)
+        n = len(nums) 
+        count = 0 
 
-        count = 0
         for i in range(n):
             if nums[i] > nums[(i+1) % n]:
-                count += 1
-        return  count <= 1         
-       
+                count += 1 
+
+        return count <= 1         
