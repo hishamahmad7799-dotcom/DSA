0003b3f39da9541b726f4ea93f17a5cdef646621
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-02-20 11:51:55 +0000

    Time: 15 ms (10.74%) | Memory: 22.4 MB (63.65%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -6,14 +6,14 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        k = k % n 
 
+        k = k % n 
         def rev(start,end):
             while start < end:
-                nums[start],nums[end] = nums[end] ,nums[start]
+                nums[start],nums[end] = nums[end],nums[start] 
                 start += 1
-                end -=1 
-        rev(0,n-1)
+                end -= 1
+        rev (0,n-1)
         rev(0,k-1)
         rev(k,n-1)        
-              
+       
