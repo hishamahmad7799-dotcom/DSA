8b16ff911c12d8aac3d7d10f398a3d635c355fd6
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-02-20 12:08:12 +0000

    Time: 3 ms (59.47%) | Memory: 14 MB (43.95%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -5,15 +5,19 @@ class Solution(object):
         :rtype: int
         """
         if not nums:
-            return 0 
-
+            return  0 
         i = 0 
-
-        for j in range (1,len(nums)):
+        for j in range(1,len(nums)):
             if nums[j] != nums[i]:
                 i += 1 
-                nums[i] = nums[j]
-        return  i+1         
+                nums[i] = nums[j] 
+        return i + 1
+
+       
+
+        
+
+
 
           
         
