2cf4c2868b60a7e0f7016deb42ccb67ff6eaa386
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-04 10:43:43 +0000

    Time: 6 ms (61.57%) | Memory: 13.7 MB (45.04%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        #1.create a temp list  
+        #1.create a temp   list  
         temp = []
 
-        # 2. temp.append remaining n!= 0 element 
+        #2.store all non zero element 
         for num in nums:
-            if  num != 0:
-                temp.append(num) 
-        # 3. count zeroes 
+            if num != 0 :
+                temp.append(num)
+        #3.count zero 
         count_zeroes = len(nums) - len(temp)
-        # 4. add zeroes t end of an array  
-        temp.extend([0]*count_zeroes)
-        # 5. copy back to original array  
+        #4.add zereos at end of an array  
+        temp.extend ([0]*count_zeroes)  
+        #5.copy back to origina array  
         for i in range(len(nums)):
-            nums[i] = temp[i]
+            nums[i] = temp[i]      
+        
                 
             
          
