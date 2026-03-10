a5b4eeb32555cd68a3c68aea1aaf4b0593dd37cb
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-10 12:29:46 +0000

    Time: 2126 ms (32.09%) | Memory: 13.3 MB (48.62%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
         # brute force 
-        n = len(nums) 
+        n = len(nums)
         for i in range(n):
             for j in range(i+1,n):
                 if nums[i] + nums[j] == target:
