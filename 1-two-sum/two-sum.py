41cd1f35a1d4be8171b6307d50b942da268a16ee
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-10 12:32:05 +0000

    Time: 4 ms (48.46%) | Memory: 13.3 MB (48.62%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # brute force 
-        n = len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i] + nums[j] == target:
-                    return[i,j]
-        
+        # optimal approach 
+        hashmap = {}
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+            if compliment in hashmap:
+                return  [hashmap[compliment],i]
+            hashmap[nums[i]] = i    
                     
 
 
