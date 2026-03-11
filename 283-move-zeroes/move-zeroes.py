fd26fb95f9b4e7f67c09dc8eb48e01d75f9ecc62
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-11 00:19:19 +0000

    Time: 4 ms (78.74%) | Memory: 13.7 MB (42.03%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j = 0  # scans the array  
+        j = 0 # scans the array  
+
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[i],nums[j] = nums[j] ,nums[i]
-                j += 1 
-        return  nums        
+                j +=1 
+
+       
 
              
 
