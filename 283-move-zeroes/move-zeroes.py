a8c4bcba99f6eb32a8a682e12af71bf4859323b7
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-12 00:33:49 +0000

    Time: 9 ms (35.08%) | Memory: 13.6 MB (41.68%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,12 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j = 0 # scans the array  
-
+        # create a temp list
+        temp = []
+        for num in nums:
+            if num != 0:
+                temp.append(num)
+        # count zero  
+        count_zero = len(nums) - len(temp)
+        temp.extend([0]*count_zero)
         for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i],nums[j] = nums[j] ,nums[i]
-                j +=1 
+            nums[i] = temp[i]
+        return nums            
 
        
 
