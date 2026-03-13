2a56d7b371b842f6bfaba7aea22102b2dc23e3c2
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-13 15:45:35 +0000

    Time: 28 ms (94.37%) | Memory: 21.9 MB (29.40%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_sum = nums[0]
+        cur_sum = 0 
+        for num in nums:
+            cur_sum += num
+
+            if cur_sum > max_sum:
+                max_sum = cur_sum
+            if cur_sum < 1:
+                cur_sum = 0 
+        return max_sum 
+                     
+        
