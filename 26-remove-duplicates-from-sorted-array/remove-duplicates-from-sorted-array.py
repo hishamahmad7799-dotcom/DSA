bc223c310341b7da8262b3dd3c3b3199de31dd97
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-22 08:37:39 +0000

    Time: 3 ms (59.41%) | Memory: 14 MB (34.77%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -13,17 +13,19 @@ class Solution(object):
         #         nums[i] = nums[j] 
         # return i + 1
 
-        # brute force 
+        # optimal approach two pointer appraoc
         if not nums:
-            return 0
-        temp = []
-        temp.append(nums[0])
-        for i in range(1,len(nums)):
-            if nums[i] != nums[i-1]:
-                temp.append(nums[i])
-        for i in range(len(temp)):
-            nums[i] = temp[i]
-        return len(temp)                 
+            return 0 
+        i = 0 
+        for j in range(1,len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return  i + 1
+                    
+       
+
+                       
 
 
 
