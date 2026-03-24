95a147beedeb8b2594e2e548b7fd81e4d43e8f55
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-03-24 09:01:00 +0000

    Time: 0 ms (100.00%) | Memory: 13 MB (76.92%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,12 +6,41 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        hashmap  = {}
-        for i in range(len(nums)):
-            compliment = target - nums[i]
+        # hashmap  = {}
+        # for i in range(len(nums)):
+        #     compliment = target - nums[i]   
+        #     if compliment in hashmap:
+        #         return [hashmap[compliment],i]
+        #     hashmap [nums[i]] = i 
+        
+        # pointer appproach  
+        # i,j = 0,len(nums)-1
+        # while i < j:
+        #     s = nums[i]+nums[j]
+        #     if s == target:
+        #         return [i ,j]
+        #     elif  s < target:
+        #         i += 1
+        #     else:
+        #         j -= 1        
+                        
+        hashmap = {}
+        for  i in range(len(nums)):
+            compliment  = target  - nums[i]
             if compliment in hashmap:
-                return [hashmap[compliment],i]
-            hashmap [nums[i]] = i              
+                return  [hashmap[compliment],i]
+            hashmap[nums[i]] = i        
+
+
+
+
+
+
+
+
+
+
+
 
 
 
