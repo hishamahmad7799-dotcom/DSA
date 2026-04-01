9f2ed6c7b6f85566db2f3886d62d3cf6e39e17b4
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-04-01 14:35:42 +0100

    Time: 3 ms (59.32%) | Memory: 13.9 MB (30.13%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,16 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # if not nums:
-        #     return  0 
-        # i = 0 
-        # for j in range(1,len(nums)):
-        #     if nums[j] != nums[i]:
-        #         i += 1 
-        #         nums[i] = nums[j] 
-        # return i + 1
-
-        # optimal approach two pointer appraoc
+        # optimize approach  
         if not nums:
             return 0 
         i = 0 
@@ -21,26 +12,9 @@ class Solution(object):
             if nums[j] != nums[i]:
                 i += 1
                 nums[i] = nums[j]
-        return  i + 1
-                    
-       
 
-                       
+        return i + 1       
 
-
-
-      
-
-    
-
+            
         
-               
-
-       
-
-        
-
-
-
-          
         
