5cfc59e7bb69dcecef0928c2c035a5f389081244
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-04-03 08:23:56 +0100

    Time: 131 ms (8.86%) | Memory: 20.8 MB (11.56%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,23 +4,20 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # opposite two pointer apprpach  
-        i = 0 # starrting   pointer 
-        j = len(height)-1 # end pointer 
+        i = 0 
+        j = len(height)-1
         max_water = 0 
-        
-        while i <j:
-            h = min(height[i],height[j])
-            w = j - i
-            area = h * w 
-            max_water = max(max_water,area)
 
-            # move the smaller pointer 
-            if height[i] < height[j]:
+        while i < j:
+            h = min(height[i],height[j])
+            w = j - i 
+            area = h*w 
+            max_water = max(max_water,area)
+            if  height[i] < height[j]:
                 i += 1
             else:
-                j -= 1
-        return max_water            
+                j -= 1    
+        return  max_water        
 
 
         
