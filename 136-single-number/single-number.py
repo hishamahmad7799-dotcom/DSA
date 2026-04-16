c153466e6a6be8bb3e3655ece5972f2c3a62a69d
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-04-16 14:06:00 +0100

    Time: 5 ms (42.16%) | Memory: 14.7 MB (9.26%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # using hashing 
+        freq = {}
+        for num in  nums:
+            freq[num] = freq.get(num,0)+1
+
+        for num in freq:
+            if freq[num] == 1:
+                return num    
+
+
+
+        
