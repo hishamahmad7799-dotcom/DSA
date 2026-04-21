5740a02cd64eff6c2f15b0057364d7f571d502e1
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-04-21 08:29:31 +0100

    Time: 0 ms (100.00%) | Memory: 21.6 MB (77.67%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
-        max_val = arr[0]
-        index = 0 
-        for i in range(len(arr)):
-            if arr[i] > max_val:
-                max_val = arr[i]
-                index = i 
-        return index        
-        
+        left,right = 0 ,len(arr)-1
+
+        while left < right:
+            mid = (left + right ) // 2
+
+            if arr[mid] < arr[mid+1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left            
