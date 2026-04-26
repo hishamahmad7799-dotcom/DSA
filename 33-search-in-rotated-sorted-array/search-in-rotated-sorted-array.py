d051af674204d3762f427f4dba9f9a6b7974d551
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-04-26 15:34:39 +0100

    Time: 0 ms (100.00%) | Memory: 12.3 MB (98.39%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,10 +5,33 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-        return  -1 
-                
+        left,right = 0,len(nums)-1
+
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid 
+
+            # left half is sorted 
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            # right half is sorted 
+
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left  = mid + 1
+                else:
+                    right = mid - 1     
+
+        return  -1               
+
+
+    
+            
+
 
         
