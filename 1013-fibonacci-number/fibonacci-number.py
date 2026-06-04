66cfcbc5b4c4798ea5d63550c14f12cb7921c68c
Hisham.ahmad <hisham.ahmad7799@gmail.com>
2026-06-04 18:26:31 +0100

    Time: 9 ms (93.66%) | Memory: 12.5 MB (16.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,16 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 0:
-           return 0
-        if n == 1:
-           return 1
+        if n <= 1:
+            return n
+        dp = [0] * (n+1)
 
-        a, b = 0, 1
-        for _ in range(2, n + 1):
-            a, b = b, a + b
-        return b
-         
+        dp[0] = 0 
+        dp[1] = 1
 
-        
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+            
         
